c9aff4a844265b6777b1554eecb5ec553fe23cfe
hwangseojin223 <seozinidev@gmail.com>
2025-05-12 09:57:59 +0900

    update main

--- backend/src/main/java/com/zoop/backend/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.zoop.backend.repository;
-
-import com.zoop.backend.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmailAndPasswordHash(String email, String passwordHash);
-}
